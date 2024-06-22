89ff0a1e57e6a140fd0ed8ac5c950e44e4fef154
Samuel Afonso <samuskas10@gmail.com>
2024-06-21 22:51:32 -0300

    Inicialização do login de usuário

--- driverplan/driverplan/settings.py
+++ driverplan/driverplan/settings.py
@@ -143,3 +143,6 @@ MEDIA_ROOT = DATA_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- driverplan/maps/views.py
+++ driverplan/maps/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from .forms import RouteForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required, permission_required
+
+
+@login_required
+def protected_view(request):
+    return render(request, 'maps/protected.html')
+
+@permission_required('app_label.permission_codename')
+def view_with_permission(request):
+    return render(request, 'maps/permission.html')
+
+def login_view(request):
+    if request.method == 'GET':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'maps/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 def index(request):
     form = RouteForm()
